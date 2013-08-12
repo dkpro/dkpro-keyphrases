12ca79177e54420f38f3f908630174dae2da45b4
pedrobssantos <pedrobssantos@7e073446-63a1-9851-1e81-bcf567796f3d>
2013-08-12 14:16:51 +0000

    Adding GPL license headers to source code from core module.

--- trunk/de.tudarmstadt.ukp.dkpro.keyphrases.core-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/type/Keyphrase.java
+++ trunk/de.tudarmstadt.ukp.dkpro.keyphrases.core-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/type/Keyphrase.java
@@ -1,6 +1,13 @@
-
-
-/* First created by JCasGen Thu Apr 18 17:45:18 CEST 2013 */
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.keyphrases.core.type;
 
 import org.apache.uima.jcas.JCas; 
