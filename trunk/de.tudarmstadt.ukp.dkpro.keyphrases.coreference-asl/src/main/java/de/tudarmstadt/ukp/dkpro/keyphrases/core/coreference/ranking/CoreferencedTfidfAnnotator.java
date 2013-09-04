22169c192ba256406d79375b78b27092e8dd9ee0
nico.erbs@gmail.com <nico.erbs@gmail.com@7e073446-63a1-9851-1e81-bcf567796f3d>
2013-09-04 15:53:27 +0000

    Working with feature path

--- trunk/de.tudarmstadt.ukp.dkpro.keyphrases.coreference-asl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/coreference/ranking/CoreferencedTfidfAnnotator.java
+++ trunk/de.tudarmstadt.ukp.dkpro.keyphrases.coreference-asl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/coreference/ranking/CoreferencedTfidfAnnotator.java
@@ -18,40 +18,57 @@
 package de.tudarmstadt.ukp.dkpro.keyphrases.core.coreference.ranking;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 
 import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceChain;
 import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceLink;
 import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathFactory;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Compound.CompoundSplitLevel;
 import de.tudarmstadt.ukp.dkpro.core.frequency.tfidf.TfidfAnnotator;
 import de.tudarmstadt.ukp.dkpro.core.frequency.tfidf.util.FreqDist;
 import de.tudarmstadt.ukp.dkpro.core.frequency.tfidf.util.TermIterator;
 
 public class CoreferencedTfidfAnnotator extends TfidfAnnotator {
 
-	@Override
-	protected FreqDist<String> getTermFrequencies(JCas jcas)
-			throws AnalysisEngineProcessException {
-		// count all terms with the given annotation
-		FreqDist<String> termFrequencies = super.getTermFrequencies(jcas);
-				
-		
-		for(CoreferenceChain chain : JCasUtil.select(jcas, CoreferenceChain.class)){
-			//TODO get lemma or token or whatever is needed
-			String term = chain.getFirst().getCoveredText();
-			
-			termFrequencies.count(term, getChainLength(chain));
-		}
-		return termFrequencies;
-	}
+    public static final String PARAM_COMPOUND_SPLIT_LEVEL = "CompoundingSplitLevel";
+    @ConfigurationParameter(name = PARAM_COMPOUND_SPLIT_LEVEL, mandatory = true, defaultValue={"ALL"})
+    private CompoundSplitLevel compoundSplitLevel;
 
-	int getChainLength(CoreferenceChain chain) {
-		int counter = 1;
-		CoreferenceLink link =chain.getFirst();
-		while((link = link.getNext()) != null){
-			counter++;
-		}
-		return counter;
-	}
+
+
+    @Override
+    protected FreqDist<String> getTermFrequencies(JCas jcas)
+            throws AnalysisEngineProcessException {
+        // count all terms with the given annotation
+        FreqDist<String> termFrequencies = super.getTermFrequencies(jcas);;
+
+
+        for(CoreferenceChain chain : JCasUtil.select(jcas, CoreferenceChain.class)){
+            //TODO get lemma or token or whatever is needed
+
+
+            for (String term : TermIterator.create(jcas, featurePath, lowercase)) {
+                termFrequencies.count(chain.getFirst().getCoveredText(), getChainLength(chain));
+                termFrequencies.count(chain.getFirst().getCoveredText(), getChainLength(chain));
+
+            }
+
+
+        }
+        return termFrequencies;
+    }
+
+    int getChainLength(CoreferenceChain chain) {
+        int counter = 1;
+        CoreferenceLink link =chain.getFirst();
+        while((link = link.getNext()) != null){
+            counter++;
+        }
+        return counter;
+    }
 }
