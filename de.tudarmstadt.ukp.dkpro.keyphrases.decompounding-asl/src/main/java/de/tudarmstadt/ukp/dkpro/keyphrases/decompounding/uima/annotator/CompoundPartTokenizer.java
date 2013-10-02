0c3a1af9d780449d4a21cc660264c19877f73cfa
Pedro Santos <pedrobssantos@gmail.com>
2013-10-02 13:30:48 +0000

    Issue 11: Prepocessing task must have lemmatizing after decompounding
    The CompoundPartTokenizer AE should assign the POS tags from the Compound token to each token created by a split.

--- de.tudarmstadt.ukp.dkpro.keyphrases.decompounding-asl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/decompounding/uima/annotator/CompoundPartTokenizer.java
+++ de.tudarmstadt.ukp.dkpro.keyphrases.decompounding-asl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/decompounding/uima/annotator/CompoundPartTokenizer.java
@@ -29,33 +29,32 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Compound.CompoundSpli
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Split;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
-@TypeCapability(
-        inputs={"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Compound",
-                "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Split"},
-        outputs={"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"})
-
+@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Compound",
+        "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Split" }, outputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token" })
 public class CompoundPartTokenizer
     extends JCasAnnotator_ImplBase
 {
 
-	/**
-	 * This annotator is type agnostic, so it is mandatory to specify the type
-	 * of the working annotation and how to obtain the string representation
-	 * with the feature path.
-	 */
-	public static final String PARAM_COMPOUND_SPLIT_LEVEL = "CompoundingSplitLevel";
-	@ConfigurationParameter(name = PARAM_COMPOUND_SPLIT_LEVEL, mandatory = true, defaultValue={"ALL"})
-	private CompoundSplitLevel compoundSplitLevel;
+    /**
+     * This annotator is type agnostic, so it is mandatory to specify the type of the working
+     * annotation and how to obtain the string representation with the feature path.
+     */
+    public static final String PARAM_COMPOUND_SPLIT_LEVEL = "CompoundingSplitLevel";
+    @ConfigurationParameter(name = PARAM_COMPOUND_SPLIT_LEVEL, mandatory = true, defaultValue = { "ALL" })
+    private CompoundSplitLevel compoundSplitLevel;
 
     @Override
     public void process(final JCas aJCas)
         throws AnalysisEngineProcessException
     {
         for (Compound compound : JCasUtil.select(aJCas, Compound.class)) {
+            final Token compoundToken = JCasUtil.selectCovered(aJCas, Token.class,
+                    compound.getBegin(), compound.getEnd()).get(0);
             for (Split compoundPart : compound.getSplitsWithoutMorpheme(compoundSplitLevel)) {
                 final Token token = new Token(aJCas);
                 token.setBegin(compoundPart.getBegin());
                 token.setEnd(compoundPart.getEnd());
+                token.setPos(compoundToken.getPos());
                 token.addToIndexes();
             }
         }
