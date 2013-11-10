5de9f1bcc9e72904bc0e5092fe26d4f07e70d810
Pedro Santos <pedrobssantos@gmail.com>
2013-11-10 15:09:36 +0000

    Issue 15: 	KeyphrasePerformanceCounter should be refactored
    Adding RprecisionKeyphraseCounter along with its unit test.

---
+++ de.tudarmstadt.ukp.dkpro.keyphrases.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/evaluator/util/RprecisionKeyphraseCounter.java
@@ -0,0 +1,39 @@
+package de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RprecisionKeyphraseCounter
+    extends AbstractKeyphraseCounter
+{
+
+    public void setRPrecision(String fileName, double rprecision)
+    {
+
+        FilePerformance filePerformance = fileName2performanceMap.get(fileName);
+        filePerformance.setRPrecision(rprecision);
+
+    }
+
+    public double getAverageRPrecision()
+    {
+        double rPrecisionSum = 0.0;
+        List<String> files = new ArrayList<String>(getRegisteredFiles());
+        for (String fileName : files) {
+            rPrecisionSum += this.getFileRPrecision(fileName);
+        }
+        return rPrecisionSum / files.size();
+    }
+
+    /**
+     * R-Precision
+     * 
+     * @param fileName
+     * @return The r-precision for that file.
+     */
+    public double getFileRPrecision(String fileName)
+    {
+        return fileName2performanceMap.get(fileName).getRPrecision();
+    }
+
+}
