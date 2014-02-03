628614b6b23cdb03afe67dfb63781d9fb0050902
Nicolais Erbs <nico@erbs.eu>
2014-02-03 22:27:09 +0000

    Fixed test case

--- de.tudarmstadt.ukp.dkpro.keyphrases.wrappers-gpl/src/test/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/wrapper/CooccurrenceGraphExtractorTest.java
+++ de.tudarmstadt.ukp.dkpro.keyphrases.wrappers-gpl/src/test/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/wrapper/CooccurrenceGraphExtractorTest.java
@@ -16,6 +16,7 @@ import static de.tudarmstadt.ukp.dkpro.keyphrases.core.wrapper.KeyphraseExtracto
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
@@ -67,14 +68,21 @@ public class CooccurrenceGraphExtractorTest
                         PosType.N
                 )
         );
+        
+        List<Keyphrase> keyphrases = cooccExtractor.extract(testDoc);
+        assertEquals(28, keyphrases.size());
 
+        List<Keyphrase> top5Keyphrases = getTopRankedUniqueKeyphrases(keyphrases, 5);
+        assertEquals(5, top5Keyphrases.size());
+        
+        
         String cooccKeyphrases = StringUtils.join(
-                keyphraseList2StringList(
-                        getTopRankedKeyphrases(cooccExtractor.extract(testDoc), 5)
-                ),
-                ","
-          );
+                keyphraseList2StringList(top5Keyphrases), ",");
+        for(Keyphrase keyphrase : top5Keyphrases){
+            System.out.println(keyphrase);
+        }
 
+        System.out.println(cooccKeyphrases);
         assertEquals(
                 "rate allocation,target bit rate,algorithm deals,bit allocation,object level",
                 cooccKeyphrases
