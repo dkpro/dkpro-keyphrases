9a9c485b6d7c5840fe9ab4c5879e7f3876c8d5d1
Nicolais Erbs <nico@erbs.eu>
2014-02-04 14:41:23 +0000

    Refactored PosPatternFilter

--- de.tudarmstadt.ukp.dkpro.keyphrases.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/filter/PosSequenceFilter.java
+++ de.tudarmstadt.ukp.dkpro.keyphrases.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/keyphrases/core/filter/PosSequenceFilter.java
@@ -12,7 +12,7 @@ import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.type.Keyphrase;
 
 public class PosSequenceFilter
@@ -20,17 +20,20 @@ extends AbstractCandidateFilter
 {
 
     public static final String PARAM_POS_PATTERNS = "posPatterns";
-    @ConfigurationParameter(name = PARAM_POS_PATTERNS, mandatory = true)
+    @ConfigurationParameter(name = PARAM_POS_PATTERNS, mandatory = false)
     private String[] posPatterns;
-    
+
     private List<String> allowedPosSequences;
 
     @Override
     public void initialize(final UimaContext context) throws ResourceInitializationException {
         super.initialize(context);
 
+        if(posPatterns == null){
+            posPatterns = getStandardPosPatterns();
+        }
+
         allowedPosSequences = Arrays.asList(posPatterns);
-//        System.out.println(allowedPosSequences);
     }
 
     @Override
@@ -38,35 +41,57 @@ extends AbstractCandidateFilter
             throws AnalysisEngineProcessException
             {
         List<Keyphrase> keyphrasesToBeRemoved = new LinkedList<Keyphrase>();
+
         for (Keyphrase keyphrase : keyphrases) {
-            String posSequence = getPosSequence(JCasUtil.selectCovered(Token.class, keyphrase));
-            
+            String posSequence = getPosSequence(JCasUtil.selectCovered(POS.class, keyphrase));
+
             if (!allowedPosSequences.contains(posSequence)) {
-//                System.out.println(keyphrase.getKeyphrase());
-//                System.out.println(posSequence);
+                //                System.out.println(keyphrase.getKeyphrase());
+                //                System.out.println(posSequence);
                 keyphrasesToBeRemoved.add(keyphrase);
             }
         }
         return keyphrasesToBeRemoved;
             }
 
-    private String getPosSequence(Collection<Token> tokens)
+    private String getPosSequence(Collection<POS> tags)
     {
         List<String> posSequence = new LinkedList<String>();
-        for (Token token : tokens) {
-            posSequence.add(token.getPos().getClass().getSimpleName());
+        for (POS tag : tags) {
+            posSequence.add(tag.getPosValue().substring(0, 1));
         }
-        return createSequence(posSequence);
-    }
 
-    public static String createSequence(String... posTags)
-    {
-        return StringUtils.join(posTags,"-");
+        return createSequence(posSequence);
     }
 
     public static String createSequence(List<String> posTags)
     {
-        return StringUtils.join(posTags,"-");
+        return StringUtils.join(posTags,"_");
+    }
+
+    public static String createSequence(String... posTags)
+    {
+        return createSequence(Arrays.asList(posTags));
+    }
+
+    public static String[] getStandardPosPatterns(){
+        return new String[]{
+                "N",
+                "N_N",
+                "A_N",
+                "V_N",
+                "N_V",
+                "N_N_N",
+                "A_N_N",
+                "A_A_N",
+                "V_N_N",
+                "V_V_N",
+                "N_P_N",
+                "N_N_N_N",
+                "A_N_N_N",
+                "A_A_N_N",
+                "A_A_A_N"
+        };
     }
 
 }
